500d77790b84ef1bd7c0dc9c02f04970b520b88f
Jorge Cisneros <fraxito@gmail.com>
2018-04-19 12:14:50 +0200

    Actualizado a v25

--- app/src/main/java/com/example/xp/repostator/Repostaje.java
+++ app/src/main/java/com/example/xp/repostator/Repostaje.java
@@ -1,11 +1,18 @@
 package com.example.xp.repostator;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -26,4 +33,31 @@ public class Repostaje extends Fragment {
         return inflater.inflate(R.layout.fragment_repostaje, container, false);
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        final EditText editPrecio = (EditText) getActivity().findViewById(R.id.precio);
+        editPrecio.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+                if(i == EditorInfo.IME_ACTION_DONE){
+                    SharedPreferences sp =
+                            getActivity().getSharedPreferences("datos", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    int size = sp.getInt("Listado_size", 0);
+                    size++;
+                    editor.putString("Repostaje_" + size, editPrecio.getText().toString());
+                    editor.putInt("Listado_size", size);
+                    editor.commit();
+                    Toast.makeText(getActivity(), editPrecio.getText().toString(), Toast.LENGTH_LONG).show();
+
+                    getActivity().finish();
+
+                }
+
+                return false;
+            }
+        });
+    }
 }
