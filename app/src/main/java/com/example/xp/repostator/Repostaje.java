ddbe2fc91441d477ef41e5f6e7238875696abf5c
Jorge Cisneros <fraxito@gmail.com>
2018-04-20 11:41:26 +0200

    Viernes v1

--- app/src/main/java/com/example/xp/repostator/Repostaje.java
+++ app/src/main/java/com/example/xp/repostator/Repostaje.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,22 +43,36 @@ public class Repostaje extends Fragment {
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
 
                 if(i == EditorInfo.IME_ACTION_DONE){
-                    SharedPreferences sp =
-                            getActivity().getSharedPreferences("datos", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sp.edit();
-                    int size = sp.getInt("Listado_size", 0);
-                    size++;
-                    editor.putString("Repostaje_" + size, editPrecio.getText().toString());
-                    editor.putInt("Listado_size", size);
-                    editor.commit();
-                    Toast.makeText(getActivity(), editPrecio.getText().toString(), Toast.LENGTH_LONG).show();
-
+                    escribeDatos();
                     getActivity().finish();
-
                 }
 
                 return false;
             }
         });
+    }
+
+    private void escribeDatos(){
+        EditText editPrecio = (EditText) getActivity().findViewById(R.id.precio);
+        EditText editKilometros = (EditText) getActivity().findViewById(R.id.kilometros);
+        EditText editLitros = (EditText) getActivity().findViewById(R.id.litros);
+        DatePicker editFecha = (DatePicker) getActivity().findViewById(R.id.fecha);
+
+        SharedPreferences sp =
+                getActivity().getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        int size = sp.getInt("Listado_size", 0);
+        size++;
+
+        editor.putString("precio_" + size, editPrecio.getText().toString());
+        editor.putString("kilometros_" + size, editKilometros.getText().toString());
+        editor.putString("litros_" + size, editLitros.getText().toString());
+        
+        editor.putInt("dia_"+size, editFecha.getDayOfMonth());
+        editor.putInt("mes_"+size, editFecha.getMonth());
+        editor.putInt("anyo_"+size, editFecha.getYear());
+
+        editor.putInt("Listado_size", size);
+        editor.commit();
     }
 }
